866b0ae41c38763bee035848bc7d8ad6af812e5f
rrmarinho <32780523+rrmarinho@users.noreply.github.com>
2022-06-13 12:07:26 -0300

    Update translate.py

--- translate.py
+++ translate.py
@@ -1,7 +1,7 @@
 from saitama_translator import *
 import sys
 
-# python .\saitama_translate.py vy5xxxxvzz650coacbsf03f2jkviwui9.joexpediagroup.com oxn009lc7n5887k96c4zfckes6uif.joexpediagroup.com pqxwwk9cyl1upnxwyqwinn0wgzui5.uber-asia.com w7irwrisb5lxwkow81udr.uber-asia.com
+# python .\translate.py vy5xxxxvzz650coacbsf03f2jkviwui9.joexpediagroup.com oxn009lc7n5887k96c4zfckes6uif.joexpediagroup.com pqxwwk9cyl1upnxwyqwinn0wgzui5.uber-asia.com w7irwrisb5lxwkow81udr.uber-asia.com
 
 if len(sys.argv) < 2:
     print (f'Please, inform the Saitama request to translate. Ex: python {sys.argv[0]} u6uosfnz3s.joexpediagroup.com')
